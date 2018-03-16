50ba7a3ab365e7311dfbc3d6693a51968b8dc260
ttionn <matt.tian@icloud.com>
2018-03-16 09:06:33 +0800

    Clean up code

--- CustomScrollView/ViewController.swift
+++ CustomScrollView/ViewController.swift
@@ -25,59 +25,28 @@ class ViewController: UIViewController {
         return .lightContent
     }
     
-    var wwdcTopConstraint: NSLayoutConstraint?
-    var wwdcHeightConstraint: NSLayoutConstraint?
-    lazy var wwdc: UIImageView = {
-        return UIImageView()
-    }()
-    
     private func setupViews() {
-//        view.addSubview(wwdc)
-//        view.addConstraints(format: "H:|[v0]-10-|", views: wwdc)
-//        view.addConstraints(format: "V:[v0(\(view.frame.width / 2))]", views: wwdc)
-//
-        
         wwdc.image = UIImage(named: "wwdc2016")
         wwdc.contentMode = .scaleAspectFill
         wwdc.clipsToBounds = true
         scrollView.addSubview(wwdc)
         
         scrollView.addConstraints(format: "H:|[v0(\(view.frame.width))]", views: wwdc)
-//        scrollView.addConstraints(format: "V:[v0(\(view.frame.width / 2))]", views: wwdc)
-        
         wwdcTopConstraint = wwdc.topAnchor.constraint(equalTo: scrollView.topAnchor)
         wwdcTopConstraint?.isActive = true
         wwdcTopConstraint?.constant = -50
-        
         wwdcHeightConstraint = wwdc.heightAnchor.constraint(equalToConstant: view.frame.width / 2)
         wwdcHeightConstraint?.isActive = true
-
-        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler))
-        swipeGesture.direction = .down
-        scrollView.addGestureRecognizer(swipeGesture)
-    }
-    
-    var isAmplifyEnabled = false {
-        didSet {
-            if isAmplifyEnabled != oldValue {
-                toggleImageSize()
-            }
-        }
-    }
-    
-    func toggleImageSize() {
-        if isAmplifyEnabled {
-            print("Enable amplifier")
-        } else {
-            print("Disable amplifier")
-        }
-    }
-    
-    @objc func swipeHandler() {
-        print(#function)
     }
     
     var previousOffSetY: CGFloat = 0
+    
+    var wwdcTopConstraint: NSLayoutConstraint?
+    var wwdcHeightConstraint: NSLayoutConstraint?
+
+    lazy var wwdc: UIImageView = {
+        return UIImageView()
+    }()
     
 }
 
@@ -85,13 +54,7 @@ extension ViewController: UIScrollViewDelegate {
 
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offSetY = scrollView.contentOffset.y
-
-        if (offSetY < -20 && offSetY > previousOffSetY) {
-            let gap = -20 - offSetY
-            let distance = offSetY - previousOffSetY
-            let unit = (wwdcHeightConstraint!.constant - view.frame.width / 2) / gap
-            wwdcHeightConstraint?.constant -= distance * unit
-        }
+        
         if offSetY <= -50 {
             wwdcTopConstraint?.isActive = false
             wwdcTopConstraint = wwdc.topAnchor.constraint(equalTo: view.topAnchor)
@@ -106,6 +69,14 @@ extension ViewController: UIScrollViewDelegate {
             wwdcTopConstraint = wwdc.topAnchor.constraint(equalTo: scrollView.topAnchor)
             wwdcTopConstraint?.isActive = true
             wwdcTopConstraint?.constant = -50
+        }
+        
+        // When scroll view goes up, re-small the image
+        if (offSetY < -20 && offSetY > previousOffSetY) {
+            let gap = -20 - offSetY
+            let distance = offSetY - previousOffSetY
+            let unit = (wwdcHeightConstraint!.constant - view.frame.width / 2) / gap
+            wwdcHeightConstraint?.constant -= distance * unit
         }
         
         previousOffSetY = offSetY
